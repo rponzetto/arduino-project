59bda07dd22122a4e16c346ec7524e0e28e6bb8c
Skrr201 <kavnedas@gmail.com>
2024-10-07 15:36:00 +0200

    Math

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,13 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  int num1;
+  int num2;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-  // THIRD TEST
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
