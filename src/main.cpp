d68bfb726fcef16e31ce14079057c24a77440596
rponzetto <rafaelponzetto3@gmail.com>
2024-10-08 11:50:19 +0200

    m2

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,52 @@
 
 
 // rafaelhjj
+// Parsa
+//adriana
+#include <Arduino.h>
+
+// Parsa
+//adriana
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int SumFunction(int x, int y);
+int multiFunction(int a, int b);
+#define LED_PIN 10 
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  int result = SumFunction(2, 3);
+  int result2 = multiFunction(2, 3);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH); // Turn the LED on 
+  delay(1000);                 // Wait for 1 second
+  digitalWrite(LED_PIN, LOW);  // Turn the LED off 
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
+//asks for 2 numbers and calculates the sum
+int SumFunction(int x, int y) {
+  if (Serial.available() > 0)
+  {
+    Serial.println("Print x: ");
+    x = Serial.parseInt();
+    Serial.println("Print y; ");
+    y = Serial.parseInt();
+  }
   return x + y;
+}
+  int multiFunction(int a, int b) {
+  if (Serial.available() > 0)
+  {
+    Serial.println("Print a: ");
+    a = Serial.parseInt();
+    Serial.println("Print b; ");
+    b = Serial.parseInt();
+  }
+  return a * b;
 }
